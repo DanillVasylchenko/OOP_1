2795685b47145d2499395784242e4fad39fb49ea
DanillVasylchenko <danillvasychenko@gmail.com>
2020-09-30 06:09:20 +0300

    kek4

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -6,7 +6,16 @@ namespace Task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введіть число типу int: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введіть число типу double: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіит число типу long: ");
+            long c = Convert.ToInt64(Console.ReadLine());
+
+            Console.WriteLine("a = {a}; b = {b}; c = {c}");
+
+
         }
     }
 }
