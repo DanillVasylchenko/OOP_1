cb7edd13f7424d16ec442ef16cb42451327d0d48
DanillVasylchenko <danillvasychenko@gmail.com>
2020-09-30 05:38:53 +0300

    second

---
+++ Task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Task_100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Привіт, ДонНУ");
+        }
+    }
+}
