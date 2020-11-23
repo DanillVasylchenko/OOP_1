c61cfea761e5988ea4b2689212ab1d143663f191
DanillVasylchenko <danillvasychenko@gmail.com>
2020-11-24 00:56:44 +0200

    kekich

--- Task_6/Program.cs
+++ Task_6/Program.cs
@@ -6,10 +6,10 @@ namespace Task_6
 {
     class massive
     {
-        private uint m;
-        private uint n;
-        private int[][] arr;
-        private int key;
+        uint m;
+        uint n;
+        int[][] arr;
+        int key;
         public void input_and_correct_data_check()
         {
             try
@@ -18,6 +18,33 @@ namespace Task_6
                 m = Convert.ToUInt32(Console.ReadLine());
                 Console.WriteLine("Input number of columns of your massive: ");
                 n = Convert.ToUInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("У Вас неадекватне значення!");
+                Environment.Exit(0);
+            }
+        }
+        public void array_creating_and_output()
+        {
+            arr = new int[m][];
+            Random random = new Random();
+            Console.WriteLine("Ваш масив, що любий серцю...");
+            for (int i = 0; i < m; i++)
+            {
+                arr[i] = new int[n];
+            
+                for (int j = 0; j < n; j++)
+                {
+                    arr[i][j] = random.Next(41);
+                }
+                Console.WriteLine($"{string.Join("  ", arr[i])}\n------------------------------");
+            }
+        }
+        public void key_input()
+        {
+            try
+            {
                 Console.WriteLine("Input a key: ");
                 key = Convert.ToInt32(Console.ReadLine());
             }
@@ -26,26 +53,12 @@ namespace Task_6
                 Console.WriteLine("У Вас неадекватне значення!");
                 Environment.Exit(0);
             }
-        }
-        public void array_creating()
-        {
-            arr = new int[m][];
-            Random random = new Random();
-            for (int i = 0; i < m; i++)
-            {
-                arr[i] = new int[n];
-            
-                for (int j = 0; j < n; j++)
-                {
-                    arr[i][j] = random.Next(40);
-                }
-            }
-          
+
         }
         public void key_max_element_with_general_output()
         {
             int max_element;
-            StringBuilder all_index = new StringBuilder();
+            StringBuilder all_max_index = new StringBuilder();
             StringBuilder all_key_equals = new StringBuilder();
             for (int i = 0; i < m; i++)
             {
@@ -54,17 +67,17 @@ namespace Task_6
                 {
                     if (arr[i][j] == max_element)
                     {
-                        all_index.Append($"[{j}] ");
+                        all_max_index.Append($"[{j}] ");
                     }
                     if (arr[i][j] == key)
                     {
-                        all_key_equals.Append($"{arr[i][j]}; ");
+                        all_key_equals.Append($"[{i}][{j}]; ");
                     }
                 }
-                Console.Write($"{string.Join("  ", arr[i])} | Max_element - {max_element}|indexes: {all_index}\nKey equals: {all_key_equals}\n");
-                all_index = new StringBuilder();
-                all_key_equals = new StringBuilder();
+                Console.WriteLine($"{string.Join("  ", arr[i])} | Max_element - {max_element} | indexes: {all_max_index}");
+                all_max_index = new StringBuilder();               
             }
+            Console.WriteLine($"\nAll key coordinates: {all_key_equals}");
         }
     }
     class Program
@@ -72,8 +85,10 @@ namespace Task_6
         static void Main(string[] args)
         {
             massive massive = new massive();
+
             massive.input_and_correct_data_check();
-            massive.array_creating();
+            massive.array_creating_and_output();
+            massive.key_input();
             massive.key_max_element_with_general_output();
         }
     }
