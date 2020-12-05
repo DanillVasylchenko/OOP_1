de4e3de2103fc2df9308520e9a8787bc13f914d2
DanillVasylchenko <danillvasychenko@gmail.com>
2020-12-05 02:17:58 +0200

    кек

--- Task_9/Program.cs
+++ Task_9/Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Linq;
 using System.Text;
+using System.Collections.Generic;
 
-namespace Task_6
+namespace Task_9
 {
     class massive
     {
-        uint m;
-        uint n;
+        static uint m;
+        static uint n;
         int[][] arr;
         int key;
+
+        public delegate bool IsEqual(int x);
+        public IsEqual ConditionCheck = x => x > 15;
+        static public void MyCalculation(int[][] arr, IsEqual func)
+        {
+            List<List<int>> full_list = new List<List<int>>();
+            List<int> row_list = new List<int>();
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (func(arr[i][j]) == true)
+                    {
+                        row_list.Add(arr[i][j]);
+                    }
+                }
+                if (row_list.Count() == 0)
+                {   
+                    continue;
+                }
+                full_list.Add(row_list);
+            }
+            arr = full_list.Select(Enumerable.ToArray).ToArray();
+        }
         public void input_and_correct_data_check()
         {
             try
@@ -25,7 +50,7 @@ namespace Task_6
                 Environment.Exit(0);
             }
         }
-        public void array_creating()
+        public int[][] array_creating()
         {
             arr = new int[m][];
             Random random = new Random();
@@ -33,12 +58,13 @@ namespace Task_6
             for (int i = 0; i < m; i++)
             {
                 arr[i] = new int[n];
-            
+
                 for (int j = 0; j < n; j++)
                 {
                     arr[i][j] = random.Next(41);
                 }
             }
+            return arr;
         }
         public void key_input()
         {
@@ -53,15 +79,15 @@ namespace Task_6
                 Environment.Exit(0);
             }
         }
-        public void max_element_with_general_output()
+        public void max_element_with_general_output(int[][] arr)
         {
             int max_element;
             StringBuilder all_max_index = new StringBuilder();
-            
-            for (int i = 0; i < m; i++)
+
+            for (int i = 0; i < arr.Length; i++)
             {
                 max_element = arr[i].Max();
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < arr[i].Length; j++)
                 {
                     if (arr[i][j] == max_element)
                     {
@@ -69,16 +95,15 @@ namespace Task_6
                     }
                 }
                 Console.WriteLine($"{string.Join("  ", arr[i])} | Max_element - {max_element} | indexes: {all_max_index}\n-----------------------------------");
-                all_max_index = new StringBuilder();               
+                all_max_index = new StringBuilder();
             }
-
         }
-        public void key_coordinates_output()
+        public void key_coordinates_output(int[][] arr)
         {
             StringBuilder all_key_equals = new StringBuilder();
-            for (int i = 0; i < m; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < arr[i].Length; j++)
                 {
                     if (arr[i][j] == key)
                     {
@@ -90,15 +115,19 @@ namespace Task_6
         }
     }
     class Program
-        {
+    {
         static void Main(string[] args)
         {
             massive massive = new massive();
             massive.input_and_correct_data_check();
-            massive.array_creating();
-            massive.max_element_with_general_output();
+            int[][] array = massive.array_creating();
+            massive.max_element_with_general_output(array);
             massive.key_input();
-            massive.key_coordinates_output();
+            massive.key_coordinates_output(array);
+
+            massive.MyCalculation(array, massive.ConditionCheck);
+            massive.max_element_with_general_output(array);
+            massive.key_coordinates_output(array);
         }
     }
 }
